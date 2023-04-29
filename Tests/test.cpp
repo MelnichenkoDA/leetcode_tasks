1985e5946d456f84b78ecf824d5772405b58c0a1
melnichenko_da <thrustdrip@gmail.com>
2023-04-29 18:10:35 +0300

    Tests

---
+++ Tests/test.cpp
@@ -0,0 +1,37 @@
+#include "pch.h"
+
+import BinarySearch;
+
+int search(const std::vector<int>& nums, int target)
+{
+	int left = 0;
+	int right = nums.size() - 1;
+	while (left <= right)
+	{
+		if (nums[left] == target)
+			return left;
+		if (nums[right] == target)
+			return right;
+
+		int mid = (left + right) >> 1;
+		int current_num = nums[mid];
+		if (current_num == target)
+			return mid;
+
+		if (current_num > target)
+			right = mid - 1;
+		else
+			left = mid + 1;
+	}
+
+	return -1;
+}
+
+TEST(BinarySearch, test1) {
+  EXPECT_EQ(search({ -1,0,3,5,9,12 }, 9), 4);
+  EXPECT_EQ(search({ -1,0,3,5,9,12 }, 2), -1);
+  EXPECT_EQ(search({ 5 }, 5), 0);
+  EXPECT_EQ(search({ 1, 3 }, 2), -1);
+
+  EXPECT_TRUE(true);
+}
